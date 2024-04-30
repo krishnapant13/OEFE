858ba9759def285fe45edf8b8df359c1215fbaa8
krishnapant13 <krishna555.pant@gmail.com>
2024-04-30 11:53:53 +0530

    server file changes

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 // export const server = "http://localhost:8000/api/v2";
 export const server = "https://oebe-w9c8.onrender.com/api/v2";
 export const frontend_url = "https://krishnapant13.github.io/OEFE";
+// export const frontend_url = "http://localhost:3000/OEFE";
 // export const backend_url = "http://localhost:8000/";
 export const backend_url = "https://oebe-w9c8.onrender.com/";
